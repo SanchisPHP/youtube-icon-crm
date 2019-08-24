25042a70291a506b5fd3728691d62130da22a8aa
SanchisSBF <weabdizain@gmail.com>
2019-08-24 09:07:11 +0800

    Create mutable configurations

--- src/Config.php
+++ src/Config.php
@@ -4,6 +4,7 @@
 namespace Solar\Panels;
 
 use Dotenv\Dotenv;
+use Solar\Panels\Models\OptionsModel;
 
 /**
  * Выборка конфигурации ресурса
@@ -30,6 +31,72 @@ class Config
     public function env(string $param)
     {
         return getenv($param);
+    }
+
+    /**
+     * Выбор параметра из базы данных
+     * @param string $option
+     * @return string
+     */
+    public function db(string $option): string
+    {
+        $modelOBJ = OptionsModel::where("options_name", $option);
+
+        if($modelOBJ->count() != 0)
+        {
+            return $modelOBJ->get()[0]->options_value;
+        }
+        exit("This parameter does not exist.");
+    }
+
+    /**
+     * Создание параметра в базе данных
+     * @param string $name
+     * @param string $value
+     * @return bool
+     */
+    public static function create(string $name, string $value): bool
+    {
+        if(OptionsModel::where("options_name", $name)->count() != 0)
+        {
+            return false;
+        }
+
+        $model                = new OptionsModel();
+        $model->options_name  = $name;
+        $model->options_value = $value;
+
+        if($model->save())
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * Обновление параметра в базе данных
+     * @param string $name
+     * @param string $value
+     * @return bool
+     */
+    public static function update(string $name, string $value): bool
+    {
+        $modelOBJ = OptionsModel::where("options_name", $name);
+        $model    = $modelOBJ->get();
+
+        if($modelOBJ->count() == 0)
+        {
+            return false;
+        }
+
+        $model[0]->options_value = $value;
+
+        if($model[0]->save())
+        {
+            return true;
+        }
+        return false;
     }
 
     /**
---
+++ src/models/OptionsModel.php
@@ -0,0 +1,17 @@
+<?php
+
+
+namespace Solar\Panels\Models;
+
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Модель конфигурации
+ * Class OptionsModel
+ * @package Solar\Panels\Models
+ */
+class OptionsModel extends Model
+{
+    protected $table = "options";
+}
